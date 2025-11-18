4a099a3a78d16e8a6d870b5aaba33bef9bdd5567
ImToggle <98242902+ImToggle@users.noreply.github.com>
2025-11-18 21:02:01 +0800

    message fading

--- src/main/java/org/polyfrost/chatting/mixin/ChatMixin.java
+++ src/main/java/org/polyfrost/chatting/mixin/ChatMixin.java
@@ -5,11 +5,14 @@ import net.minecraft.client.gui.components.ChatComponent;
 import org.polyfrost.chatting.core.ModConfig;
 import org.polyfrost.chatting.core.Util;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+//#if MC <= 1.21.5
+//$$ import com.llamalad7.mixinextras.injector.wrapoperation.*;
+//#endif
 
 //#if MC <= 1.16.5
 //$$ import net.minecraft.network.chat.Component;
@@ -54,4 +57,49 @@ public abstract class ChatMixin {
     @Inject(method = "render", at = @At("TAIL"))
     private void postRender(CallbackInfo ci) {
     }
+
+    //Chat Message Fading
+
+    //#if MC <= 1.21.5
+    //$$ @ModifyConstant(method = "render", constant = @Constant(intValue = 200))
+    //$$ private int setFadeTime(int value) {
+    //$$     return (int) Math.ceil(20 * ModConfig.INSTANCE.getFadeTime());
+    //$$ }
+    //$$
+        //#if MC > 1.16.5
+        //$$ @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/GuiMessage$Line;addedTime()I"))
+        //$$ private int toggleFade(net.minecraft.client.GuiMessage.Line instance,
+        //#elseif MC == 1.16.5
+        //$$ @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/GuiMessage;getAddedTime()I"))
+        //$$ private int toggleFade(net.minecraft.client.GuiMessage<?> instance,
+        //#else
+        //$$ @WrapOperation(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ChatLine;getUpdatedCounter()I"))
+        //$$ private int toggleFade(net.minecraft.client.gui.ChatLine instance,
+        //#endif
+    //$$     Operation<Integer> original, @Local(ordinal = 0, argsOnly = true) int ticks) {
+    //$$     if (ModConfig.INSTANCE.getFade()) {
+    //$$         return original.call(instance);
+    //$$     } else {
+    //$$         return ticks;
+    //$$     }
+    //$$ }
+    //#endif
+
+    //#if MC >= 1.16.5
+    @Inject(method = "getTimeFactor", at = @At("HEAD"), cancellable = true)
+    private static void toggleFade(CallbackInfoReturnable<Double> cir) {
+        if (!ModConfig.INSTANCE.getFade()) cir.setReturnValue(1.0);
+    }
+
+    @ModifyConstant(method = "getTimeFactor", constant = @Constant(doubleValue = 200.0))
+    private static double setFadeTime(double value) {
+        return 20 * ModConfig.INSTANCE.getFadeTime();
+    }
+    //#else
+    //$$
+    //$$ @ModifyConstant(method = "drawChat", constant = @Constant(doubleValue = 200.0))
+    //$$ private double setFadeTime(double value) {
+    //$$     return 20 * ModConfig.INSTANCE.getFadeTime();
+    //$$ }
+    //#endif
 }
