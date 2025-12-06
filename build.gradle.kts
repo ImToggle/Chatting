6483c5719b8e00d8d64a127ee31dd66da2adb3dd
ImToggle <98242902+ImToggle@users.noreply.github.com>
2025-12-06 19:28:12 +0800

    bump OneConfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 toolkitLoomHelper {
     useOneConfig {
-        version = "1.0.0-alpha.177"
+        version = "1.0.0-alpha.180"
         loaderVersion = "1.1.0-alpha.53"
         usePolyMixin = true
         polyMixinVersion = "0.8.4+build.6"
